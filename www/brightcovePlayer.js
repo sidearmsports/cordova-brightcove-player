d6c011888feea497d03c3a8988d577b00af46da6
Joe Flateau <joe@joeflateau.net>
2020-09-25 09:31:01 -0400

    change brightcove video js interface's name

--- www/brightcovePlayer.js
+++ www/brightcovePlayer.js
@@ -1,6 +1,7 @@
 var exec = require("cordova/exec");
 
-exports.play = function (videoId, adConfigId, adTagId, success, error) {
+exports.play = function (options, success, error) {
+  const { videoId, adConfigId, adTagId } = options;
   exec(success, error, "BrightcovePlayer", "play", [
     videoId,
     adConfigId,
